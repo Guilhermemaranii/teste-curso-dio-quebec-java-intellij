488a39cf79bff1f6199f03bdf4f24be9def00291
Guilherme Marani <90596972+Guilhermemaranii@users.noreply.github.com>
2022-10-09 18:14:44 -0300

    added TipoDeDados package

---
+++ src/br/com/dio/quebec/tiposdedados/TiposDeDados.java
@@ -0,0 +1,60 @@
+package br.com.dio.quebec.tiposdedados;
+
+import java.sql.SQLOutput;
+
+public class TiposDeDados {
+
+    public static void main(String[] args) {
+
+        byte b1 = -50;
+        byte b2 = 20;
+
+        short s1 = 32552;
+        short s2 = 22552;
+
+        int i1 = 5000;
+        int i2 = 2000;
+
+        long l1 = 1000000999L;
+        long l2 = 2000000999L;
+
+        float f1 = 999999999F;
+        float f2 = 299999999F;
+
+        double dd1 = 15.99D;
+        double dd2 = 14.11D;
+
+        char c1 = 'D';
+        char c2 = 'E';
+        char c3 = '\u0050';
+
+        String j1 = "TATU";
+        String j2 = "TATURANA pulando A CERCA para P3GAR UM M0R4NG0";
+        String j3 = "TATURANA pulando A CERCA para P3GAR UM M0R4NG0 %¨#$@#$@#%¨&@¨#@$%#@";
+
+        Boolean t1 = false;
+        Boolean t2 = true;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(dd1);
+        System.out.println(dd2);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(j1);
+        System.out.println(j2);
+        System.out.println(j3);
+        System.out.println(t1);
+        System.out.println(t2);
+
+    }
+}
